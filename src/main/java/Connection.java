a397648e7dda3f89859cfaf15ab0d0971f8cfde4
danielbras <carloosdd9550@gmail.com>
2020-10-10 17:58:45 -0300

    Added connection test servlet

---
+++ src/main/java/Connection.java
@@ -0,0 +1,33 @@
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+@WebServlet(name = "HelloServlet", urlPatterns = "/ConnectDB")
+public class Connection extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        java.sql.Connection connection = null;
+        try {
+            connection = ConnectDatabase.getConnection();
+        } catch (SQLException ex) {
+            response.getWriter().append("Falha na conexão! Verifique o console de saída");
+        }
+
+        if(connection!=null) {
+            response.getWriter().append("A conexão com o banco foi realizada!");
+             try {
+                connection.close();
+            } catch (SQLException throwables) {
+                throwables.printStackTrace();
+            }
+        } else {
+            response.getWriter().append("A conexão com o banco falhou!");
+        }
+    }
+}
