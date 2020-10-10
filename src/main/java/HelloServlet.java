3e961cad5338d2f1457b32768bcf597b8d17bd6e
danielbras <carloosdd9550@gmail.com>
2020-10-10 14:36:33 -0300

    Hello servlet test

---
+++ src/main/java/HelloServlet.java
@@ -0,0 +1,17 @@
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "HelloServlet", urlPatterns = "/HelloServlet")
+public class HelloServlet extends HttpServlet {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String nome = request.getParameter("nome");
+        PrintWriter out = response.getWriter();
+
+        out.println("Olá " + nome);
+    }
+}
