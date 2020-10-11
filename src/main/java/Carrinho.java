61200fa5d432182591c85e9db74789b1dae27bfa
danielbras <carloosdd9550@gmail.com>
2020-10-11 12:44:44 -0300

    Added the carrinho class

---
+++ src/main/java/Carrinho.java
@@ -0,0 +1,37 @@
+import java.util.ArrayList;
+
+public class Carrinho {
+    private ArrayList<Produto> produtos;
+
+    public Carrinho(ArrayList<Produto> produtos) {
+        super();
+        this.produtos = produtos;
+    }
+
+    public ArrayList<Produto> getProdutos() {
+        return produtos;
+    }
+
+    public void setProdutos(ArrayList<Produto> produtos) {
+        this.produtos = produtos;
+    }
+
+    public Produto getProduto (int id){
+        Produto mp = null;
+        for (Produto p : produtos){
+            if (p.getId() == id){
+                return p;
+            }
+        }
+        return mp;
+    }
+
+    public void removeProduto (int id){
+        Produto p = getProduto(id);
+        produtos.remove(p);
+    }
+
+    public void addProduto (Produto p){
+        produtos.add(p);
+    }
+}
