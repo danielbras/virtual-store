08f7ea27a100c593eec80e00b41414b1052ebb57
danielbras <carloosdd9550@gmail.com>
2020-10-11 12:44:32 -0300

    Added the produto class

---
+++ src/main/java/Produto.java
@@ -0,0 +1,60 @@
+public class Produto {
+    private int id;
+    private int preco;
+    private String nome;
+    private String Descricao;
+    private int estoque;
+
+    public Produto(int id, int preco, String nome, String descricao, int estoque) {
+        super();
+        this.id = id;
+        this.preco = preco;
+        this.nome = nome;
+        this.Descricao = descricao;
+        this.estoque = estoque;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getPreco() {
+        return preco;
+    }
+
+    public void setPreco(int preco) {
+        this.preco = preco;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return Descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        Descricao = descricao;
+    }
+
+    public int getEstoque() {
+        return estoque;
+    }
+
+    public void incrementaEstoque() {
+        this.estoque++;
+    }
+
+    public void diminuiEstoque() {
+        this.estoque--;
+    }
+}
