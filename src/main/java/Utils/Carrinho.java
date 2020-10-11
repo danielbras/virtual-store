e8e8181f60144f541ab3b055ec248518c17d4088
danielbras <carloosdd9550@gmail.com>
2020-10-11 20:50:46 -0300

    Comment cart

--- src/main/java/Utils/Carrinho.java
+++ src/main/java/Utils/Carrinho.java
@@ -5,37 +5,37 @@ import Utils.Produto;
 import java.util.ArrayList;
 
 public class Carrinho {
-    private ArrayList<Produto> produtos;
-
-    public Carrinho(ArrayList<Produto> produtos) {
-        super();
-        this.produtos = produtos;
-    }
-
-    public ArrayList<Produto> getProdutos() {
-        return produtos;
-    }
-
-    public void setProdutos(ArrayList<Produto> produtos) {
-        this.produtos = produtos;
-    }
-
-    public Produto getProduto (int id){
-        Produto mp = null;
-        for (Produto p : produtos){
-            if (p.getId() == id){
-                return p;
-            }
-        }
-        return mp;
-    }
-
-    public void removeProduto (int id){
-        Produto p = getProduto(id);
-        produtos.remove(p);
-    }
-
-    public void addProduto (Produto p){
-        produtos.add(p);
-    }
+//    private ArrayList<Produto> produtos;
+//
+//    public Carrinho(ArrayList<Produto> produtos) {
+//        super();
+//        this.produtos = produtos;
+//    }
+//
+//    public ArrayList<Produto> getProdutos() {
+//        return produtos;
+//    }
+//
+//    public void setProdutos(ArrayList<Produto> produtos) {
+//        this.produtos = produtos;
+//    }
+//
+//    public Produto getProduto (int id){
+//        Produto mp = null;
+//        for (Produto p : produtos){
+//            if (p.getId() == id){
+//                return p;
+//            }
+//        }
+//        return mp;
+//    }
+//
+//    public void removeProduto (int id){
+//        Produto p = getProduto(id);
+//        produtos.remove(p);
+//    }
+//
+//    public void addProduto (Produto p){
+//        produtos.add(p);
+//    }
 }
