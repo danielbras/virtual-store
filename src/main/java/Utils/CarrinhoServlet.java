fd560b4ccc780d7db2b10e40463e752c75c70df1
danielbras <carloosdd9550@gmail.com>
2020-10-12 16:34:44 -0300

    Added instruction to add product in cart

--- src/main/java/Utils/CarrinhoServlet.java
+++ src/main/java/Utils/CarrinhoServlet.java
@@ -1,15 +1,34 @@
 package Utils;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 @WebServlet(name = "VerCarrinhoServlet", urlPatterns = {"/Carrinho"})
 public class CarrinhoServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        response.getWriter().println("Adiciona produto");
+        String nomeProduto = request.getParameter("nome");
+        String comando = request.getParameter("command");
+
+        Produto produto = ProdutoSQL.buscar(nomeProduto);
+
+        HttpSession session = request.getSession(false);
+
+        if(comando.equals("add")) {
+            session.setAttribute("nome", produto.getNome());
+            session.setAttribute("descricao", produto.getDescricao());
+            session.setAttribute("preco", produto.getPreco());
+        //  TODO Atulizar estoque no BD
+        } else if(comando.equals("remove")) {
+
+        }
+
+        RequestDispatcher encaminhar = request.getRequestDispatcher("/ListaProdutos.jsp");
+        encaminhar.forward(request, response);
     }
 }
