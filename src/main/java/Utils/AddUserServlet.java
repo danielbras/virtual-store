ce8ea78951dcbd4b7cb4551e14e997ae5935efc6
danielbras <carloosdd9550@gmail.com>
2020-10-12 14:44:07 -0300

    Updated addUser servlet

--- src/main/java/Utils/AddUserServlet.java
+++ src/main/java/Utils/AddUserServlet.java
@@ -10,7 +10,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet(name = "Utils.AddUserServlet", urlPatterns = {"/AddUser"})
+@WebServlet(name = "Utils.AddUserServlet", urlPatterns = {"/AddUsuario"})
 public class AddUserServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         String nome = request.getParameter("nome");
