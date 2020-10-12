03cf7494b18eb2ee9583fba82c0deca6ddba369f
danielbras <carloosdd9550@gmail.com>
2020-10-12 10:33:08 -0300

    fixed error

--- src/main/java/Authentication.java
+++ src/main/java/Authentication.java
@@ -18,8 +18,8 @@ public class Authentication extends HttpServlet {
         Usuario usuario = UsuarioSQL.buscar(email);
 
         if(email.equals(usuario.getEmail()) && senha.equals(usuario.getSenha())) {
-            if(usuario.getTipo() == "Cliente"){
-                response.sendRedirect("/ListaProdutos.jsp");
+            if(usuario.getTipo().equals("Cliente")){
+                response.sendRedirect("/ListaProdutos.jsp?tipo=Cliente");
             } else {
                 response.sendRedirect("/ListaProdutos.jsp");
             }
