bda5f3c88970b42e93b34a6e69e1c7cb6155075e
danielbras <carloosdd9550@gmail.com>
2020-10-12 15:18:56 -0300

    Updated view cart servlet

--- src/main/java/Utils/VerCarrinhoServlet.java
+++ src/main/java/Utils/VerCarrinhoServlet.java
@@ -1,3 +1,5 @@
+package Utils;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -5,7 +7,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet(name = "VerCarrinhoServlet", urlPatterns = {"/ViewCart"})
+@WebServlet(name = "VerCarrinhoServlet", urlPatterns = {"/VerCarrinho"})
 public class VerCarrinhoServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
