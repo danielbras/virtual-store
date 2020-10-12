3c4e873d30bcdcc6a18deb5ab19995a13f874d4e
danielbras <carloosdd9550@gmail.com>
2020-10-12 16:08:45 -0300

    Updated cart servlet

--- src/main/java/Utils/CarrinhoServlet.java
+++ src/main/java/Utils/CarrinhoServlet.java
@@ -7,13 +7,9 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet(name = "VerCarrinhoServlet", urlPatterns = {"/VerCarrinho"})
-public class VerCarrinhoServlet extends HttpServlet {
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
-    }
-
+@WebServlet(name = "VerCarrinhoServlet", urlPatterns = {"/Carrinho"})
+public class CarrinhoServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+        response.getWriter().println("Adiciona produto");
     }
 }
